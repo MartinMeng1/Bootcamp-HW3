121c8e054d25e0d0b9a9f956c01d96a6badeac63
MartinMeng1 <126767342+MartinMeng1@users.noreply.github.com>
2023-04-06 20:42:09 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+ let passwordLength = parseInt(prompt("How long do you want your password to be?"));
+ if(passwordLength < 8 || passwordLength > 128){
+  alert("Error, invalid password length, the length of the password has to be between 8 characters to 128 characters");
+  return " "
+ }
+
+ var lowerCase = confirm("Do you want to include lowercase characters in your password");
+ var upperCase = confirm("Do you want to include uppercase character in your password");
+ var numeric = confirm("Do you want to include numbers in your password");
+ var specialCharacter = confirm("Do you want to include special characters to your password");
+
+ if(!lowerCase && !upperCase && !numeric && !specialCharacter){
+  alert("Error,invalid selection of character types, at least one character type should be selected");
+  return " "
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
